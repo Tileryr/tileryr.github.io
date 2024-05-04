14b73fc51e64294638201c3a0a9f66ef2ae03cbc
Tileryr <147273486+Tileryr@users.noreply.github.com>
2024-05-04 12:46:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-var cookies = 0;
+var pencils = 0;
+var shivs =  0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("pencils").innerHTML = cookies;
+function pencilClick(number){
+    pencils = pencils + number
+    document.getElementById("pencils").innerHTML = pencils;
 };
+
+function buyShiv(){
+	var shivCost = Math.floor(100 * Math.pow(1.1,shivs));
+	if(pencils >= shivCost){
+		shivs = shivs + 1;
+		pencils = pencils - shivCost;
+		document.getElementById("pencils").innerHTML = pencils;
+		document.getElementById("shivs").innerHTML = shivs;
+	};
+	var nextCost = Math.floor(100 * Math.pow(1.1,shivs))
+	document.getElementById("shivCost").innerHTML = nextCost;
+}
+
+window.setInterval(function(){
+	pencilClick(shivs * 5);
+}, 1000);
